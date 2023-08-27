50e6676f834dd34c542bf0166ec7ac9a090efe36
tharu <tharucse@gmail.com>
2023-08-26 22:45:10 -0700

    Add files via upload

---
+++ eRegistrar2/src/main/java/edu/mum/cs/cs425/demowebapps/eregistrar/controller/HomePageController.java
@@ -0,0 +1,14 @@
+package edu.mum.cs.cs425.demowebapps.eregistrar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+	@GetMapping(value= {"/","/eregistrar","/eregistrar/home"})
+	public String displayHomePage()
+	{
+		return "home/index";
+	}
+
+}
---
+++ eRegistrar2/src/test/java/edu/mum/cs/cs425/demowebapps/eregistrar/ERegistrar2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.cs425.demowebapps.eregistrar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERegistrar2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
